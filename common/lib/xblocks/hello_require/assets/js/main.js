faf5e6a4598f947d9a4b143d75a74051e5cb6a11
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-10-14 18:17:38 +0300

    JS files

---
+++ common/lib/xblocks/hello_require/assets/js/main.js
@@ -0,0 +1,9 @@
+define(['hello_require/assets/js/utils'], function(Utils){
+    function HelloRequire(runtime, element) {
+        Utils.show_message("Hello RequireJS");
+    }
+
+    return {
+        init: HelloRequire
+    };
+});
