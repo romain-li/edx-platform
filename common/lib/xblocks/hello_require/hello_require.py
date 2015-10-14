f5e642a48fb612a14c0e0f8a007c009ba74c6fef
E. Kolpakov <eugeny.kolpakov@gmail.com>
2015-10-14 20:48:11 +0300

    Student view and workbench scenarios

--- common/lib/xblocks/hello_require/hello_require.py
+++ common/lib/xblocks/hello_require/hello_require.py
@@ -1,5 +1,18 @@
 from xblock.core import XBlock
+from xblock.fragment import Fragment
 
 
 class HelloRequireXBlock(XBlock):
-    pass
+    def student_view(self, context):
+        fragment = Fragment()
+
+        fragment.add_javascript_url(self.runtime.local_resource_url(self, 'assets/js/main.js'))
+        fragment.initialize_js('xblock-hello-require/assets/js/main.js', use_require_js=True)
+
+        return fragment
+
+    @staticmethod
+    def workbench_scenarios():
+        return [
+            ("Hello require", """<xblock-hello-require/>""")
+        ]
